d1d52c1a1e850797a71c3c9432e183086767282d
Sven Grunewaldt <strayer@olle-orks.org>
2021-09-08 16:37:51 +0200

    Add diff and diagnostics to lualine

--- lua/config-lualine.lua
+++ lua/config-lualine.lua
@@ -2,7 +2,18 @@ local M = {}
 
 M.cfg = function()
   THEME = require('theme')
-  require('lualine').setup({options = {theme = THEME.active_theme}})
+  require('lualine').setup({
+    options = {theme = THEME.active_theme},
+    sections = {
+      lualine_a = {'mode'},
+      lualine_b = {'branch'},
+      lualine_c = {'diff', {'diagnostics', sources = {'nvim_lsp'}}, 'filename'},
+      lualine_x = {'encoding', 'fileformat', 'filetype'},
+      lualine_y = {'progress'},
+      lualine_z = {'location'}
+    },
+    extensions = {'fugitive', 'nvim-tree'}
+  })
 end
 
 return M
