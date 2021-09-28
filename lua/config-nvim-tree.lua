cae27538345676c098ca106d313dc6269d585003
Sven Grunewaldt <strayer@olle-orks.org>
2021-09-28 21:58:12 +0200

    Update nvim-tree config

--- lua/config-nvim-tree.lua
+++ lua/config-nvim-tree.lua
@@ -4,12 +4,10 @@ M.cfg = function()
   vim.api.nvim_set_keymap('n', '<Leader>d', '<CMD>NvimTreeToggle<CR>', {noremap = true, silent = true})
 
   vim.g.nvim_tree_quit_on_open = 1
-  vim.g.nvim_tree_lsp_diagnostics = 1
-  vim.g.nvim_tree_disable_netrw = 0
   vim.g.nvim_tree_indent_markers = 1
   vim.g.nvim_tree_hide_dotfiles = 1
-  vim.g.nvim_tree_follow = 1
-  vim.g.nvim_tree_auto_close = 1
+
+  require'nvim-tree'.setup({lsp_diagnostics = true, disable_netrw = false, auto_close = true, update_focused_file = {enable = true}})
 end
 
 return M
