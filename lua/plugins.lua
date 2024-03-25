d6fc451fe789c404486e7995c4daa1b471a81cca
Sven Grunewaldt <s.grunewaldt@westfalen.com>
2024-03-25 16:43:46 +0100

    Lazy-load neotree

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -67,6 +67,7 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v2.x",
+    cmd = "Neotree",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
