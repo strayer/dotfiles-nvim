a51c02c8935731bc65dcb42dd4dca40e174769be
Sven Grunewaldt <s.grunewaldt@westfalen.com>
2024-03-27 09:18:45 +0100

    Update lspkind.nvim cmp config

--- lua/config-cmp.lua
+++ lua/config-cmp.lua
@@ -66,20 +66,14 @@ M.cfg = function()
       },
     },
     formatting = {
-      format = function(entry, vim_item)
-        -- fancy icons and a name of kind
-        vim_item.kind = require("lspkind").presets.default[vim_item.kind] .. " " .. vim_item.kind
-
-        -- set a name for each source
-        vim_item.menu = ({
-          buffer = "[Buffer]",
-          nvim_lsp = "[LSP]",
-          luasnip = "[LuaSnip]",
-          nvim_lua = "[Lua]",
-          latex_symbols = "[Latex]",
-        })[entry.source.name]
-        return vim_item
-      end,
+      format = require("lspkind").cmp_format({
+        mode = 'symbol', -- show only symbol annotations
+        maxwidth = 50,   -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+        -- can also be a function to dynamically calculate max width such as
+        -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+        ellipsis_char = '...',    -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+        show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+      })
     },
   })
 end
