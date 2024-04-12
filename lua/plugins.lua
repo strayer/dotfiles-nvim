41b2808d43e0e512db09ddd146c4c4bc10537a38
Sven Grunewaldt <s.grunewaldt@westfalen.com>
2024-04-12 13:12:32 +0200

    Fix leap.nvim keybindings

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -39,7 +39,6 @@ return {
   {
     "ggandor/leap.nvim",
     dependencies = { "tpope/vim-repeat" },
-    event = "VeryLazy",
     config = function()
       require("leap").set_default_keymaps()
     end,
