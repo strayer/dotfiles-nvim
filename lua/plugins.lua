3fa97391eccad4461175aa4e1206d826950c4e9f
Sven Grunewaldt <s.grunewaldt@westfalen.com>
2024-11-21 10:41:52 +0100

    fix(completions): change keymap preset to `super-tab`

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -107,7 +107,7 @@ return {
       -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
       -- see the "default configuration" section below for full documentation on how to define
       -- your own keymap.
-      keymap = { preset = "default" },
+      keymap = { preset = "super-tab" },
 
       highlight = {
         -- sets the fallback highlight groups to nvim-cmp's highlight groups
