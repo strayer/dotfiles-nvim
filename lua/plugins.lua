6a99f47cf02a08139311f1825e2a3167638f9e4b
Sven Grunewaldt <s.grunewaldt@westfalen.com>
2024-12-12 11:18:00 +0100

    fix: explicitly set storm as tokyonight style
    I'm not sure why I didn't realize this before... I've been using the
    storm style in other applications already and neovim apparently didn't?

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -191,11 +191,7 @@ return {
     lazy = false,
     priority = 1000,
     opts = {
-      transparent = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
+      style = "storm",
     },
   },
   {
