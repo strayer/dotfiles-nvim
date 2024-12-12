a94206fdd9f57325247be37344e33507a065cffe
Sven Grunewaldt <s.grunewaldt@westfalen.com>
2024-12-12 11:15:06 +0100

    chore: update blink.cmp

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -113,18 +113,27 @@ return {
         -- sets the fallback highlight groups to nvim-cmp's highlight groups
         -- useful for when your theme doesn't support blink.cmp
         -- will be removed in a future release, assuming themes add support
-        use_nvim_cmp_as_default = true,
+        use_nvim_cmp_as_default = false, -- tokyonight supports blink
       },
       -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
       -- adjusts spacing to ensure icons are aligned
       nerd_font_variant = "normal",
 
+      -- default list of enabled providers defined so that you can extend it
+      -- elsewhere in your config, without redefining it, via `opts_extend`
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+      },
+
       -- experimental auto-brackets support
-      -- accept = { auto_brackets = { enabled = true } }
+      -- completion = { accept = { auto_brackets = { enabled = true } } }
 
       -- experimental signature help support
-      -- trigger = { signature_help = { enabled = true } }
+      -- signature = { enabled = true }
     },
+    -- allows extending the providers array elsewhere in your config
+    -- without having to redefine it
+    opts_extend = { "sources.default" },
   },
   -- TODO: validate what features of lspsaga I'm actually using
   {
