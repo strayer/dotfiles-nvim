0397ddd6cc8e3c0bfcf30b684a5635af84a93e61
Sven Grunewaldt <strayer@olle-orks.org>
2025-01-21 10:23:27 +0100

    feat(formatting): add FormatToggle command

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -342,7 +342,13 @@ return {
       default_format_opts = {
         lsp_format = "fallback",
       },
-      format_on_save = { timeout_ms = 500 },
+      format_on_save = function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        return { timeout_ms = 500 }
+      end,
       formatters = {
         shfmt = {
           prepend_args = { "-i", "2", "-bn" },
@@ -369,6 +375,35 @@ return {
       require("conform.util").add_formatter_args(yaml_formatter, { "--print-width", "120" }, { append = false })
       ---@cast yaml_formatter conform.FormatterConfigOverride
       require("conform").formatters.prettier_yaml = yaml_formatter
+    end,
+    init = function()
+      local notify = require("notify")
+
+      local function show_notification(message, level)
+        notify(message, level, { title = "conform.nvim" })
+      end
+
+      vim.api.nvim_create_user_command("FormatToggle", function(args)
+        local is_global = not args.bang
+        if is_global then
+          vim.g.disable_autoformat = not vim.g.disable_autoformat
+          if vim.g.disable_autoformat then
+            show_notification("Autoformat-on-save disabled globally", "info")
+          else
+            show_notification("Autoformat-on-save enabled globally", "info")
+          end
+        else
+          vim.b.disable_autoformat = not vim.b.disable_autoformat
+          if vim.b.disable_autoformat then
+            show_notification("Autoformat-on-save disabled for this buffer", "info")
+          else
+            show_notification("Autoformat-on-save enabled for this buffer", "info")
+          end
+        end
+      end, {
+        desc = "Toggle autoformat-on-save",
+        bang = true,
+      })
     end,
   },
   {
