0730e0375237091bed736dd3905ab95085a2daf2
Sven Grunewaldt <strayer@olle-orks.org>
2025-04-25 11:35:35 +0200

    feat(mini.nvim): enable mini.diff

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -400,6 +400,7 @@ return {
       require("mini.indentscope").setup()
       require("mini.icons").setup()
       require("mini.surround").setup()
+      require("mini.diff").setup()
 
       MiniIcons.mock_nvim_web_devicons()
     end,
