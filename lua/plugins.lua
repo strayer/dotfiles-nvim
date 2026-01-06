54fcad29c23f608052e21101191d4d38646842bb
Sven Grunewaldt <strayer@olle-orks.org>
2026-01-06 13:52:22 +0100

    feat: add claudecode.nvim, replace CodeCompanion
    Add coder/claudecode.nvim for Claude Code CLI integration with Neovim.
    Remove codecompanion.nvim in favor of the official IDE integration.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -567,32 +567,6 @@ return {
     opts = {},
   },
   {
-    "olimorris/codecompanion.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-      -- "hrsh7th/nvim-cmp", -- Optional: For using slash commands and variables in the chat buffer
-      -- "nvim-telescope/telescope.nvim", -- Optional: For using slash commands
-      { "stevearc/dressing.nvim", opts = {} }, -- Optional: Improves `vim.ui.select`
-      "j-hui/fidget.nvim",
-    },
-    opts = require("config-codecompanion").config(),
-    init = function()
-      require("plugins.codecompanion.fidget-spinner"):init()
-    end,
-    keys = {
-      {
-        "<leader>ac",
-        "<cmd>CodeCompanionChat Toggle<cr>",
-        desc = "CodeCompanion: chat",
-        mode = { "n", "v" },
-        { noremap = true, silent = true },
-      },
-      { "<C-a>", "<cmd>CodeCompanionActions<cr>", mode = { "n", "v" }, { noremap = true, silent = true } },
-      { "ga", "<cmd>CodeCompanionChat Add<cr>", mode = "v", { noremap = true, silent = true } },
-    },
-  },
-  {
     "fladson/vim-kitty",
     ft = "kitty",
   },
@@ -608,6 +582,30 @@ return {
         markdown = true,
         help = true,
       },
+    },
+  },
+  {
+    "coder/claudecode.nvim",
+    dependencies = { "folke/snacks.nvim" },
+    config = true,
+    keys = {
+      { "<leader>a", nil, desc = "AI/Claude Code" },
+      { "<leader>ac", "<cmd>ClaudeCode<cr>", desc = "Toggle Claude" },
+      { "<leader>af", "<cmd>ClaudeCodeFocus<cr>", desc = "Focus Claude" },
+      { "<leader>ar", "<cmd>ClaudeCode --resume<cr>", desc = "Resume Claude" },
+      { "<leader>aC", "<cmd>ClaudeCode --continue<cr>", desc = "Continue Claude" },
+      { "<leader>am", "<cmd>ClaudeCodeSelectModel<cr>", desc = "Select Claude model" },
+      { "<leader>ab", "<cmd>ClaudeCodeAdd %<cr>", desc = "Add current buffer" },
+      { "<leader>as", "<cmd>ClaudeCodeSend<cr>", mode = "v", desc = "Send to Claude" },
+      {
+        "<leader>as",
+        "<cmd>ClaudeCodeTreeAdd<cr>",
+        desc = "Add file",
+        ft = { "NvimTree", "neo-tree", "oil", "minifiles", "netrw" },
+      },
+      -- Diff management
+      { "<leader>aa", "<cmd>ClaudeCodeDiffAccept<cr>", desc = "Accept diff" },
+      { "<leader>ad", "<cmd>ClaudeCodeDiffDeny<cr>", desc = "Deny diff" },
     },
   },
   {
